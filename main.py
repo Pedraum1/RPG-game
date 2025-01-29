fe3301759b9ca41f81bac129209c868deb58ebed
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-01-29 19:21:59 -0300

    FEAT: Implement combat simulation between Warrior and Mage characters

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hello World")
+from Classes.Roles import Warrior,Mage
+from Classes.Items import BlackCleaver
+import time, os
+
+
+Garen = Warrior('Garen',20)
+Ryze = Mage('Ryze',20)
+blackCleaver =  BlackCleaver()
+Garen.items.append(blackCleaver)
+
+os.system('cls')
+
+print(Ryze.lifeBar)
+print(Garen.lifeBar)
+time.sleep(1)
+
+print('begin X1')
+
+time.sleep(1)
+os.system('cls')
+
+while Garen.HP > 0 and Ryze.HP > 0:
+
+    Ryze.autoAttack(Garen)
+    Garen.lifeBar.update(Garen)
+
+    print(Ryze.lifeBar)
+    print(Garen.lifeBar)
+
+    time.sleep(0.5)
+    os.system('cls')
+
+    Garen.autoAttack(Ryze)
+
+    print(Ryze.lifeBar)
+    print(Garen.lifeBar)
+
+    time.sleep(0.5)
+    os.system('cls')
