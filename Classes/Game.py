f12d6a099d6de2056ed6b08c742db46deb510cf9
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-02-02 20:27:04 -0300

    FEAT: Implement difficulty settings and game run functionality in Game class

--- Classes/Game.py
+++ Classes/Game.py
@@ -3,17 +3,42 @@ from Functions.Prompt import *
 
 class Game():
     def __init__(self):
-        self.difficulty = 0
+        self.difficulty = "Easy"
+        self.difficulty_level = 0
+        self.difficulty_levels = ["Easy","Medium","Hard"]
 
     def menu(self) -> int:
-        menu_options = ['New Game', 'Settings', 'Exit']
+        clearPrompt()
+        menu_options = ['New Game', 'Settings']
         return printOptions(menu_options,"PYTHON RPG")
     
-    def start():
-        pass
+    def run(self) -> None:
+        while True:
+            print("Imagine the game running ...")
+            anyKeyPressed()
+            clearPrompt()
+            break
 
-    def settings():
-        pass
+    def settings(self) -> None:
+        clearPrompt()
+        settings_options = [f"Difficulty: {self.difficulty}"]
+        option = printOptions(settings_options,"Settings")
+
+        if option == 0:
+            clearPrompt()
+            new_difficulty = printOptions(self.difficulty_levels,"Difficulty")
+
+            if new_difficulty == len(self.difficulty_levels):
+                clearPrompt()
+                self.settings()
+            else:
+                self.difficulty = self.difficulty_levels[new_difficulty]
+                self.difficulty_level = self.difficulty
+                clearPrompt()
+                self.settings()
+
+        if option == len(settings_options):
+            clearPrompt()
     
     def exit(self) -> None:
         exit()
