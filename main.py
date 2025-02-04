1f20da09903d995c64fee55169ff250a80213e1d
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-02-03 22:57:15 -0300

    FEAT: Replace Assassin with Tank class and update combat logic

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import time, os
-from Classes.Roles import Assassin, Warrior
+from Classes.Roles import Tank, Warrior
 from Classes.Items import *
 from Functions.inputs import anyKeyPressed 
 
-Zed = Assassin('Zed',3)
+Ornn = Tank('Ornn',3)
 Garen = Warrior('Garen',3)
 
 item1 = BlackCleaver()
-#item2 = BlackCleaver()
 item3 = Thornmail()
 
-Zed.gainItem(item1)
-#Garen.gainItem(item2)
-Garen.gainItem(item3)
+Ornn.gainItem(item3)
+Garen.gainItem(item1)
 
 os.system('cls')
 
 print(Garen.lifeBar)
-print(Zed.lifeBar)
+print(Ornn.lifeBar)
 time.sleep(1)
 
 print('begin X1')
@@ -25,20 +23,20 @@ print('begin X1')
 time.sleep(1)
 os.system('cls')
 
-while Zed.hp > 0 and Garen.hp > 0:
+while Ornn.hp > 0 and Garen.hp > 0:
 
     print(Garen.lifeBar)
-    print(Zed.lifeBar)
-    Zed.autoAttack(Garen)
+    print(Ornn.lifeBar)
+    Ornn.autoAttack(Garen)
     Garen.lifeBar.update(Garen)
 
     anyKeyPressed()
     os.system('cls')
 
     print(Garen.lifeBar)
-    print(Zed.lifeBar)
-    Garen.autoAttack(Zed)
-    Zed.lifeBar.update(Zed)
+    print(Ornn.lifeBar)
+    Garen.autoAttack(Ornn)
+    Ornn.lifeBar.update(Ornn)
 
     anyKeyPressed()
     os.system('cls')
