64e1f436aa3129c77b63c88da380dd34fda681e3
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-02-03 22:57:59 -0300

    FEAT: Enhance gameplay functions with random factor and type hints for clarity

--- Functions/gameplay.py
+++ Functions/gameplay.py
@@ -1,5 +1,10 @@
-def percent(value:int, origin:float):
+from random import randint
+
+def percent(value:int, origin:float) -> float:
     return origin*value/100
 
-def calculateReduction(armor):
+def calculateReduction(armor:float) -> float:
     return 100/(100+armor)
+
+def randomFactor() -> int:
+    return randint(0, 100)
